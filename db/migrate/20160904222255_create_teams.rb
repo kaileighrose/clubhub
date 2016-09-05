5c6902c16d75a6e497d3d29fc74a772f6da8d161
kaileighrose <kaileighrose@gmail.com>
2016-09-04 20:27:50 -0400

    config omniauth in devise, generate welcome controller, create welcome#index view, add name to users

--- db/migrate/20160904222255_create_teams.rb
+++ db/migrate/20160904222255_create_teams.rb
@@ -3,7 +3,7 @@ class CreateTeams < ActiveRecord::Migration
     create_table :teams do |t|
       t.string :name
       t.boolean :active
-      t.datetime :next_meeting
+      t.integer :meeting_id
       t.integer :coach_id
 
       t.timestamps null: false
