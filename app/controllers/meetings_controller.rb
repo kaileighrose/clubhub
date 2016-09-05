d3e1a80c3f21a96352b3083d9f44482cc6f131eb
kaileighrose <kaileighrose@gmail.com>
2016-09-04 23:54:53 -0400

    set up basic crud actions for Meetings controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,37 @@
 class MeetingsController < ApplicationController
+  
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.create(meeting_params)
+    redirect_to meeting_path(@meeting)
+  end
+
+  def edit
+    @meeting = Meeting.find(params[:id])
+  end
+
+  def update
+    @meeting = Meeting.find(params[:id])
+  end
+
+  def show
+    @meeting = Meeting.find(params[:id])
+  end
+
+  def index
+    @meetings = Meeting.all
+  end
+
+  def destroy
+    @meeting = Meeting.find(params[:id])
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:team_id, :space_id, :time)
+  end
 end
